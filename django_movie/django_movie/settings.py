198ac6e901ac50ce47f3da954a4ad9e754610f45
forza111 <nikitos.ionkin@yandex.ru>
2021-01-31 23:39:22 +0300

    static

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static123/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static123')
 STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
