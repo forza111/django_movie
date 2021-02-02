189d7cea61987250e50c6daa61163992ce23c6a3
forza111 <nikitos.ionkin@yandex.ru>
2021-02-02 12:38:07 +0300

    Изменение views.py View --> ListView, DetailView

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
+
 
 from .models import Movie
 
-class MoviesView(View):
+class MoviesView(ListView):
     '''Список фильмов'''
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list':movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movies/movies.html'
 
-class MovieDetailView(View):
+
+class MovieDetailView(DetailView):
     '''Полное описание фильма'''
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, "movies/movie_detail.html", {"movie": movie})
+    model = Movie
+    slug_field = 'url' #по какому полю нужно искать нашу запись
+    #template_name джанго в данном случае генерирует автоматически имя модели Movie + detail
+
