c2ba489d490ec25f8a2f960280a7020df97bf13d
forza111 <nikitos.ionkin@yandex.ru>
2021-02-17 17:28:08 +0300

    Добавлене рейтнга к фильму Rev.01

--- django_movie/movies/templatetags/movie_tag.py
+++ django_movie/movies/templatetags/movie_tag.py
@@ -11,6 +11,6 @@ def get_categories():
     return Category.objects.all()
 
 @register.inclusion_tag('movies/tags/last_movie.html')
-def get_last_movies(count=5):
-    movies = Movie.objects.order_by("id")[:count]
+def get_last_movies(count=1):
+    movies = Movie.objects.order_by("-id")[:count]
     return {"last_movies": movies}
